3c610aba48f1ed37a4ec79e2d58d9196bacfb2cf
Duncan Gordon <dgordon@liquidnet.com>
2020-05-18 15:57:16 +0100

    Add a global git ignore that doesn't namespace clash with the .gitignore in this directory

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 read -p "This will nuke all the dotfiles and replace them with symlinks. Ok? (y/n) " -n 1;
 echo "";
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    FILES=(.ackrc .aliases .bash_complete .bash_profile .bash_prompt .bashrc .curlrc .docker_aliases .docker_bash .exports .functions .fzf.bash .gitconfig .hushlogin .inputrc .minimalvimrc .screenrc .todo .vimrc .vim)
+    FILES=(.ackrc .aliases .bash_complete .bash_profile .bash_prompt .bashrc .curlrc .docker_aliases .docker_bash .exports .functions .fzf.bash .gitconfig .gitignore_global .hushlogin .inputrc .minimalvimrc .screenrc .todo .vimrc .vim)
     cd "$(dirname "${BASH_SOURCE}")"
     for file in ${FILES[@]}; do rm -rf ../$file; done
     for file in ${FILES[@]}; do ln -s $HOME/dotfiles/$file $HOME/$file; done
