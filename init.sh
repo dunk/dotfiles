e17874625b2d56e6a91a3e1e5dc9b531ba2f804a
Duncan Gordon <dgordon@liquidnet.com>
2020-12-18 21:17:16 +0000

    add a todo to the init

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ read -p "This will nuke all the dotfiles and replace them with symlinks. Ok? (y/
 echo "";
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     FILES=(.ackrc .aliases .bash_complete .bash_profile .bash_prompt .bashrc .curlrc .docker_aliases .docker_bash .exports .functions .fzf.bash .gitconfig .gitignore_global .hushlogin .inputrc .minimalvimrc .screenrc .todo .vimrc .vim)
-    cd "$(dirname "${BASH_SOURCE}")"
+    cd "$(dirname "${BASH_SOURCE}")" # TODO: do this just once??
     for file in ${FILES[@]}; do rm -rf ../$file; done
     for file in ${FILES[@]}; do ln -s $HOME/dotfiles/$file $HOME/$file; done
 fi
